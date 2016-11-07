e572b019148aaf6f332a9d3064b4da726b404da1
Babajide Kale <bkale09@gmail.com>
2016-11-07 11:47:44 -0500

    deploy

--- src/Utils/Helpers.js
+++ src/Utils/Helpers.js
@@ -3,6 +3,6 @@ export default {
 
 searchAuthenticJobs: function(data){
   console.log("Backend");
-    return axios.post('https://floating-everglades-94662.herokuapp.com/techjobs',data)
+    return axios.post('http://floating-everglades-94662.herokuapp.com/techjobs',data)
   }
 }
